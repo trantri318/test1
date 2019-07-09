506b1aa24bc1acaffef015f5e0676eac66e8ec91
“Tri” <trantrilong318@gmail.com>
2019-07-09 14:54:30 +0700

    create new, index, show, and table category

--- app/controllers/laptops_controller.rb
+++ app/controllers/laptops_controller.rb
@@ -1,7 +1,46 @@
-class LatopsController < ApplicationController
-
+class LaptopsController < ApplicationController
+    before_action :set_laptop, only: [:show, :edit, :update, :destroy]
   def index
     @laptops = Laptop.all
   end
-  
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @laptop.update(laptop_params)
+      redirect_to laptops_url
+    else
+      redirect_to edit_laptop_url(@laptop.id)
+    end
+  end
+
+  def new
+    @laptop = Laptop.new
+  end
+
+  def create
+    @laptop = Laptop.new(laptop_params)
+    if @laptop.save
+      redirect_to laptops_url
+    else
+      redirect_to new_laptop_url
+    end 
+      
+  end
+    private
+    
+  def set_laptop
+    @laptop = Laptop.find(params[:id])
+  end
+
+  def laptop_params
+    params.require(:laptop).permit(:title, :description, :price, :published)
+  end
+
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_043111) do
+ActiveRecord::Schema.define(version: 2019_07_09_075221) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "laptops", force: :cascade do |t|
     t.string "title"
